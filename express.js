bdd593a9d36719a3ea7bc40215f57d8aff77c13f
Rafa Lozano <rafael@avena.io>
2021-08-20 23:14:05 -0500

    feat: use body-parser *create a simple documentation request, add post, get all, and find user by email

--- express.js
+++ express.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-const users =[
+app.use(express.json())
+
+let users =[
     {name:'Rafa', date:new Date, email:'rafa@gmail.com'},
     {name:'Gus', date:new Date, email:'gus@gmail.com'},
 ]
@@ -22,6 +24,28 @@ app.get('/api/users/:email',(request, response) => {
         response.status(404).end()
 
     }
+})
+app.delete('/api/users/:email', (request, response) =>{
+    const email = request.params.email
+    users = users.filter( user => user.email !== email)
+    response.status(204).end();
+})
+app.post('/api/users',(request,response)=>{
+    const user = request.body
+    console.log(user)
+    if(!user || !user.name){
+        return response.status(400).json({
+            error:'user.name is missing'
+        })
+    }
+    const newUser = {
+        name:user.name,
+        email:user.email,
+        date:new Date().toISOString()
+    }
+
+    users.push(newUser)
+    response.json(newUser)
 })
 
 const PORT = 3001
